8dcde98075c10037ea8abf0064cbac88e3715a7f
l3miage-seveniej <theblaireau1@hotmail.fr>
2022-03-24 15:38:51 +0100

    finalisation

--- src/main/java/fr/uga/im2ag/l3/miage/db/MenuUtilisateur.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/MenuUtilisateur.java
@@ -331,28 +331,7 @@ public class MenuUtilisateur {
         Bornette bornette;
         Location location;
 
-        // Liste des Bornettes
-        // Station station = new Station();
-
-        // Bornette b1 = new Bornette(Etat.OK, station);
-        // Bornette b2 = new Bornette(Etat.OK, station);
-        // Bornette b3 = new Bornette(Etat.HS, station);
-
-        // Velo v1 = new Velo(Modele.VTC, Etat.OK, Situation.EN_STATION, new Date(2020,
-        // 1, 1), b1);
-        // Velo v2 = new Velo(Modele.VTT, Etat.OK, Situation.EN_STATION, new Date(2020,
-        // 1, 4), b2);
-        // Velo v3 = new Velo(Modele.HOLLANDAIS, Etat.HS, Situation.EN_STATION, new
-        // Date(2020, 2, 9), b3);
-
-        // b1.setVelo(v1);
-        // b2.setVelo(v2);
-        // b3.setVelo(v3);
-
-        // station.setBornettes(Arrays.asList(b1, b2, b3));
-
-        // Fin Liste des Bornettes
-        // codeSecret = scanner.nextLine();
+        
 
         if( c instanceof Abonne){
         codeSecret = LectureClavier.lireEntier("Saisir votre code secret : ");
