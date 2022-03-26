dfe5bec438f4c5ccbeee944721b95097d90f114b
l3miage-wicaksop <pradityo.wicaksono@etu.univ-grenoble-alpes.fr>
2022-03-26 04:31:20 +0100

    creation d'une classe MenuAdministrateur

--- src/main/java/fr/uga/im2ag/l3/miage/db/App.java
+++ src/main/java/fr/uga/im2ag/l3/miage/db/App.java
@@ -8,7 +8,7 @@ import fr.uga.im2ag.l3.miage.db.utils.LectureClavier;
 public class App {
 
     public static void main(String[] args) {
-        
+
         System.out.println("##############################################");
         System.out.println(" | _ \\_ _ ___ (_)___| |_  | _ )   \\|   \\ ");
         System.out.println(" |  _/ '_/ _ \\| / -_)  _| | _ \\ |) | |) |");
@@ -16,59 +16,65 @@ public class App {
         System.out.println("            |__/                         ");
         System.out.println("##############################################");
         System.out.println("");
-        while(true){
+        while (true) {
             int choix = 0;
-            while (choix <1 || choix >2){
-                choix = LectureClavier.lireEntier("Choisissez votre choix!\n1. Mode Client\n2. Mode Administrateur");
+            while (choix < 1 || choix > 3) {
+                choix = LectureClavier.lireEntier(
+                        "Choisissez votre choix!\n1. Mode Client\n2. Mode Administrateur\n3. Quitter l'application");
                 System.out.println("");
             }
 
-            switch(choix){
-                case 1: 
+            switch (choix) {
+                case 1:
                     MenuUtilisateur menu = new MenuUtilisateur();
-                    
+
                     menu.mainMenu();
                     break;
                 case 2:
                     System.out.println("");
-                    Scenarios scenarios = new Scenarios();
-                    int choixMenuAdmin = LectureClavier.lireEntier("Saisissez\n1 pour reset database\n 2 pour peupler la base des données! ");
-                    while(choixMenuAdmin<1 || choixMenuAdmin>2){
-                        choixMenuAdmin = LectureClavier.lireEntier("Saisissez\n1 pour reset database\n 2 pour peupler la base des données! ");
+                    MenuAdministrateur menuAdmin = new MenuAdministrateur();
+                    int choixMenuAdmin = LectureClavier
+                            .lireEntier("Saisissez\n1 pour reset database\n2 pour peupler la base des données! ");
+                    while (choixMenuAdmin < 1 || choixMenuAdmin > 2) {
+                        choixMenuAdmin = LectureClavier
+                                .lireEntier("Saisissez\n1 pour reset database\n2 pour peupler la base des données! ");
                     }
-                    switch(choixMenuAdmin){
-                        case 1: 
-                            scenarios.resetDatabase();
+                    switch (choixMenuAdmin) {
+                        case 1:
+                            menuAdmin.resetDatabase();
                             break;
                         case 2:
                             try {
-                                scenarios.peupler();
+                                menuAdmin.peupler();
                             } catch (ParseException e) {
                                 // TODO Auto-generated catch block
                                 e.printStackTrace();
                             }
                             break;
 
-
                     }
-                    
+                    break;
+                case 3:
+                    System.out.println("Au revoir!");
+                    System.exit(0);
+                    break;
+
             }
         }
 
-        
         /*
-        try {
-            scenario2();
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            scenario3();
-        } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        */
+         * try {
+         * scenario2();
+         * } catch (ParseException e) {
+         * // TODO Auto-generated catch block
+         * e.printStackTrace();
+         * }
+         * try {
+         * scenario3();
+         * } catch (ParseException e) {
+         * // TODO Auto-generated catch block
+         * e.printStackTrace();
+         * }
+         */
     }
 }
